f4773ebcdc8a97ce6cd2bc824d343b8e9a7c89c7
Yirui Li <77665425+Mar-lll@users.noreply.github.com>
2025-07-14 14:02:08 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 
 from src import config
-from src.ENERF_SLAM import ENerF_SLAM
+from src.ENeRF_SLAM import ENeRF_SLAM
 
 
 def setup_seed(seed):
@@ -27,16 +27,13 @@ def main():
                         help='input folder, this have higher priority, can overwrite the one in config file')
     parser.add_argument('--output', type=str,
                         help='output folder, this have higher priority, can overwrite the one in config file')
-    nice_parser = parser.add_mutually_exclusive_group(required=False)
-    #nice_parser.add_argument('--nice', dest='nice', action='store_true')
-    #nice_parser.add_argument('--imap', dest='nice', action='store_false')
-    #parser.set_defaults(nice=True)
+    parser.set_defaults(nice=False)
     args = parser.parse_args()
 
     cfg = config.load_config(
         args.config, 'configs/enerf_slam.yaml')
 
-    slam = ENerF_SLAM(cfg, args)
+    slam = ENeRF_SLAM(cfg, args)
 
     slam.run()
 
